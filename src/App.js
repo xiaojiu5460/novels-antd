e0b7865dee0cec89b4b30dc5847f2f405af857b5
tanlin <123456a>
2021-01-05 17:31:01 +0800

    首页跑马灯展示

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import './App.css';
-import {Button} from 'antd-mobile'
+import { Button } from 'antd-mobile'
 import 'antd-mobile/dist/antd-mobile.css'
 
 function App() {
   return (
     <div className="App">
       <Button type="primary">This is a button</Button>
-      {/*<header className="App-header">*/}
-        {/*<p>*/}
-          {/*Edit <code>src/App.js</code> and save to reload.*/}
-        {/*</p>*/}
-        {/*<a*/}
-          {/*className="App-link"*/}
-          {/*href="https://reactjs.org"*/}
-          {/*target="_blank"*/}
-          {/*rel="noopener noreferrer"*/}
-        {/*>*/}
-          {/*Learn React*/}
-        {/*</a>*/}
-      {/*</header>*/}
+
     </div>
   );
 }
